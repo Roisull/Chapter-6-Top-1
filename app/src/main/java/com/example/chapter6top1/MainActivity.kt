f57dcdbdc4c3d0aba5957a9ae069cef8069bf747
lusiro <lusiro2001@students.amikom.ac.id>
2022-10-11 20:26:01 +0700

    two

--- app/src/main/java/com/example/chapter6top1/MainActivity.kt
+++ app/src/main/java/com/example/chapter6top1/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.chapter6top1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.chapter6top1.databinding.ActivityMainBinding
+import com.example.chapter6top1.viewModel.ViewModelBlur
+import com.example.chapter6top1.viewModel.ViewModelBlurFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val viewModel: ViewModelBlur by viewModels { ViewModelBlurFactory(application) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnGo.setOnClickListener {
+            viewModel.applyBlur(1)
+        }
     }
 }
